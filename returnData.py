56f378683f036aaa7367a8534bc5cbbee894bb05
Maksym Topchaniuk <m.topchaniuk@levi9.com>
2017-02-09 18:38:58 +0200

    draft of return data

--- returnData.py
+++ returnData.py
@@ -2,21 +2,50 @@ import fetchURL
 #import parseHTML
 from bs4 import BeautifulSoup
 import sys
+import re
 
+resourse = 'http://equipboard.com'
+fetchResults = []
+equipment = []
+pile = []
 
-fetchClass = fetchURL.FetchURL()
-#http://equipboard.com/role/guitarists
-#http://equipboard.com/role/bassists
-fetchClass.fetch('http://equipboard.com/role/drummers')
+fetchClass1 = fetchURL.FetchURL()
+fetchClass2 = fetchURL.FetchURL()
+#/role/guitarists
+#/role/bassists
+#/role/drummers
+fetchClass1.fetch(resourse + '/role/bassists')
 #print fetchClass.responseHTML
 #print sys.stdout.encoding
 
 
-soup = BeautifulSoup(fetchClass.responseHTML, "html.parser")
+soup1 = BeautifulSoup(fetchClass1.responseHTML, "html.parser")
 #parser = parseHTML.MyHTMLParser()
 #parser.feed(fetchClass.responseHTML)
-names = soup.find_all('h2')
+names = soup1.find_all('a', attrs={'href' : re.compile("^/pros/(?!new)")})
 for name in names:
     if name:
-        print name.text.encode('utf-8')
+        fetchResults.append(resourse + name.get('href'))
+        #print name.get('href')
+        #.text.encode('utf-8')
+#print fetchResults
+#print names
 
+
+for result in fetchResults:
+    if result:
+        fetchClass2.fetch(result)
+        pile.append(fetchClass2.responseHTML)
+
+#print fetchClass2.responseHTML
+
+soup2 = BeautifulSoup(pile, "html.parser")
+
+
+
+equips = soup2.find_all('h3')
+for equip in equips:
+    if equip:
+        equipment.extend(equip)
+
+print equipment
